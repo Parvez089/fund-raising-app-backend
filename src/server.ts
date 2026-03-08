4b4a693a67d14df7f59329a36a7d70d9d763af2b
parvez <mhparvez089@gmail.com>
2026-03-08 14:53:53 +0600

    fix: add vercel domain to CORS

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "https://fund-raising-hatbair.vercel.app",
+      "https://fund-raising-lilac.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
